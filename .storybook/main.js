74c6c69e74fb176b963707d3a206ae151482ac26
Maximilian Leith <maximilian@leith.de>
2021-12-19 17:38:02 +0100

    storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,19 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+    stories: [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-knobs",
+        "@storybook/addon-backgrounds",
+        "@storybook/addon-measure",
+        "@storybook/addon-outline",
+    ],
+    framework: "@storybook/react",
+    core: {
+        builder: "webpack5",
+    },
+};
