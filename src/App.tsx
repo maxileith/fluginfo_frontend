610205efb8f7c934ad9014258fe6b385906ffbac
Maximilian Leith <maximilian@leith.de>
2022-01-03 16:48:03 +0100

    Toast

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ import Layout from "./pages/Layout";
 import OfferDetails from "./pages/OfferDetails";
 import OfferSearch from "./pages/OfferSearch";
 import Status from "./pages/Status";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./toastify.scss";
 
 export default function App() {
     return (
@@ -30,6 +33,21 @@ function NavigateWrapper(): JSX.Element {
                 <Route path="/status" element={<Status />} />
                 <Route path="*" element={<Navigate to="/offer/search" />} />
             </Routes>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={7000}
+                hideProgressBar={false}
+                pauseOnHover={true}
+                pauseOnFocusLoss={true}
+                closeOnClick={false}
+                newestOnTop={true}
+                draggable={true}
+                draggablePercent={60}
+                draggableDirection="x"
+                limit={5}
+                toastStyle={{ backgroundColor: "#fafafa" }}
+                style={{ marginBottom: "2.5rem" }}
+            />
         </Layout>
     );
 }
