067e9950a59a0e3ff9016fd57ff158864e9f8a94
Maximilian Leith <maximilian@leith.de>
2022-01-20 23:45:21 +0100

    corrected docker stuff

--- docker-save.sh
+++ docker-save.sh
@@ -1,1 +1,1 @@
-docker save maxileith/fluginfo-frontend:latest | gzip > maxileith_fluginfo-backend_latest.tar.gz
+docker save maxileith/fluginfo-frontend:latest | gzip > maxileith_fluginfo-frontend_latest.tar.gz
