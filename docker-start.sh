f393c6088e881a581b10a23d57d9a79c3f7a825b
Maximilian Leith <maximilian@leith.de>
2022-01-31 00:12:04 +0100

    fix bugs on windows

--- docker-start.sh
+++ docker-start.sh
@@ -1,2 +1,1 @@
-[[ -z "${FLUGINFO_BACKEND_BASE_URL}" ]] || echo ${FLUGINFO_BACKEND_BASE_URL} > /usr/share/nginx/html/backendBaseUrl.txt
-nginx -g "daemon off;"
+[[ -z "${FLUGINFO_BACKEND_BASE_URL}" ]] || echo ${FLUGINFO_BACKEND_BASE_URL} > /usr/share/nginx/html/backendBaseUrl.txt && nginx -g "daemon off;"
