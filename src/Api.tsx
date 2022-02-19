5f04d0fb66ea91537fbded6c727cb32012e82209
Maximilian Leith <maximilian@leith.de>
2022-02-19 11:35:56 +0100

    axios send credentials

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://localhost:8000" });
+const instance = axios.create({
+    baseURL: "http://localhost:8000",
+    withCredentials: true,
+});
 
 export default instance;
